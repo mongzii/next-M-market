b51a75e607ba0fd5d5b236ad084c8dc32e08dd68
moonjiyoung <umarg228@naver.com>
2024-02-16 18:17:37 +0900

    feat: 상단바생성

--- app/page.js
+++ app/page.js
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <h4 className="title">애플후레시</h4>
+      <p className="title-sub">by Moon</p>
+    </div>
+  );
 }
