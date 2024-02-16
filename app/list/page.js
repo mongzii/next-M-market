03ae2b0ab1de3ca4bfcff9fb1d84aa1b75f68d5b
moonjiyoung <umarg228@naver.com>
2024-02-16 19:12:14 +0900

    feat: list image

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,28 @@
 export default function List() {
+  let 상품 = ["Tomatoes", "Pasta", "Coconut"];
+
   return (
     <div>
       <h2 className="title">상품목록</h2>
-      <div className="food">
-        <h4>상품1 $40</h4>
+
+      {상품.map((a, i) => {
+        return (
+          <div className="food" key={i}>
+            <img src={`/jacket${i + 1}.png`} className="food-img" />
+            <h4>{상품[i]} $40</h4>
+          </div>
+        );
+      })}
+
+      {/* <div className="food">
+        <h4>{상품[0]} $40</h4>
       </div>
       <div className="food">
-        <h4>상품2 $40</h4>
+        <h4>{상품[1]} $40</h4>
       </div>
+      <div className="food">
+        <h4>{상품[2]} $40</h4>
+      </div> */}
     </div>
   );
 }
