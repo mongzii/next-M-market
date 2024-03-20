79b295185cfad70511e9676a29e4b8cb50c3262e
moonjiyoung <umarg228@naver.com>
2024-03-20 17:01:44 +0900

    feat: shoe페이지생성

--- app/list/shoe/page.js
+++ app/list/shoe/page.js
@@ -1,3 +1,61 @@
+"use client";
+
+import ListLeft from "@/app/ListLeft";
+import { useState } from "react";
+
 export default function ShoePage() {
-  return <div>shoe페이지</div>;
+  let 상품 = [
+    "Tomatoes",
+    "Pasta",
+    "Coconut",
+    "Tomatoes",
+    "Pasta",
+    "Coconut",
+    "Tomatoes",
+    "Pasta",
+    "Coconut",
+  ];
+  let [수량, 수량변경] = useState([2, 2, 2, 2, 2, 2, 2, 1, 1]);
+
+  return (
+    <div>
+      <h1 className="title">Shoe</h1>
+      <div className="merchandise">
+        <div className="list-left">
+          <ListLeft />
+        </div>
+        <div className="list-right">
+          <div className="goods">
+            {상품.map((a, i) => {
+              return (
+                <div className="food" key={i}>
+                  <img src={`/shoe${i + 1}.png`} className="food-img" />
+                  <h4>{상품[i]} $40</h4>
+                  <button
+                    onClick={() => {
+                      let minuscopy = [...수량];
+                      minuscopy[i]--;
+                      수량변경(minuscopy);
+                    }}
+                  >
+                    -
+                  </button>
+                  <span> {수량[i]} </span>
+                  <button
+                    onClick={() => {
+                      let copy = [...수량];
+                      copy[i]++;
+                      수량변경(copy);
+                    }}
+                  >
+                    +
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
