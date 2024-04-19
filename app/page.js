a70bb795d04bf61114867610ee277db8108eee37
moonjiyoung <umarg228@naver.com>
2024-04-19 16:42:41 +0900

    feat: 캐러셀구현중

--- app/page.js
+++ app/page.js
@@ -8,7 +8,39 @@ export default async function Home() {
 
   return (
     <div>
-      <h4 className="title">애플후레시</h4>
+      <h2 className="title">M-market</h2>
+      <div className="main">
+        <div className="main-top">
+          <div className="slide-container">
+            <div className="slide-box">
+              <img src="/main1.png" />
+            </div>
+
+            <div className="slide-box">
+              <img src="/main11.jpg" />
+            </div>
+            <div className="slide-box">
+              <img src="/main12.jpg" />
+            </div>
+            <div className="slide-box">
+              <img src="/main13.jpg" />
+            </div>
+
+            <div className="slide-box">
+              <img src="/main14.jpg" />
+            </div>
+          </div>
+        </div>
+        <div className="main-middle">
+          <img src="/main2.jpg" />
+          <img src="/main3.jpg" />
+
+          <img src="/main5.jpg" />
+        </div>
+        <div className="main-bottom">
+          <img src="/main4.jpg" />
+        </div>
+      </div>
     </div>
   );
 }
