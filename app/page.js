f8e529ce330aa54f7a3ae122ea6922ab88172367
moonjiyoung <umarg228@naver.com>
2024-04-21 22:10:25 +0900

    design: main photo정리

--- app/page.js
+++ app/page.js
@@ -11,32 +11,10 @@ export default async function Home() {
     <div>
       <h1 className="title">M-market</h1>
       <div className="main">
-        {/* <div className="main-top">
-          <div className="slide-container">
-           
-            <div className="slide-box">
-              <img src="/main1.png" />
-            </div>
-            <div className="slide-box">
-              <img src="/main11.jpg" />
-            </div>
-            <div className="slide-box">
-              <img src="/main12.jpg" />
-            </div>
-            <div className="slide-box">
-              <img src="/main13.jpg" />
-            </div>
-            <div className="slide-box">
-              <img src="/main14.jpg" />
-            </div>
-
-          </div>
-        </div> */}
         <Slide />
         <div className="main-middle">
           <img src="/mainbt2.jpg" />
           <img src="/mainbt3.jpg" />
-
           <img src="/mainbt5.jpg" />
         </div>
         <div className="main-bottom">
